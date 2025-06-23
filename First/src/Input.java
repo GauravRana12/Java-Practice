bcece060d0630d00a92d499a57b5a6bfc1956990
GauravRana12 <gauravp1335@gmail.com>
2025-06-23 12:46:30 +0530

    input pratice

---
+++ First/src/Input.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.util.Scanner;
+public class Input {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Please Enter Your Name: ");
+        String name=sc.nextLine();
+        System.out.println("Welcome " + name + ", How you doing?");
+
+    }
+}
